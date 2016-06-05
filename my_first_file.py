63e503b240e8cb1172b9813edb6fed6eb28ac38a
Mike Lindner <mike@israeltechallenge.com>
2016-06-05 19:00:11 +0300

    Explain output

--- my_first_file.py
+++ my_first_file.py
@@ -1,7 +1,7 @@
 
 def sample_func(x, y, z):
-	print x
-	print y
-	print z
+	print "x: " + str(x)
+	print "y: " + str(y)
+	print "z: " + str(z)
 	
-	print x + y + z
+	print "x + y + z: " + str(x + y + z)
